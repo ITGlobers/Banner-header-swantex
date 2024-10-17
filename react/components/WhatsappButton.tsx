79ae7859e562c7e629fe0090215525c04eb30c15
unknown <elgatobanban@gmail.com>
2024-10-17 11:25:31 -0500

    feat: whatsapp button added

---
+++ react/components/WhatsappButton.tsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useCssHandles } from 'vtex.css-handles'
+import './styles.css'
+
+type Props = {
+  logo: string // WhatsApp.png
+  text: string // texto al lado del icono
+  phone: string // 3507064545
+  message: string // Estás comunicándote con VTEX University, por favor ingresa tu duda.
+  width: number // 80px
+  height: number // 80px
+}
+
+const WhatsappButton = ({
+  logo,
+  text,
+  phone,
+  message,
+  width,
+  height,
+}: Props) => {
+  const CSS_HANDLES = [
+    'containerWhatsapp',
+    'containerLink',
+    'containerIcon',
+    'containerText',
+  ]
+
+  const handles = useCssHandles(CSS_HANDLES)
+  const formattedMessage = message.replace(/ /g, '%20')
+
+  return (
+    <>
+      <div className={handles.containerWhatsapp}>
+        <a
+          href={`https://wa.me/${phone}?text=${formattedMessage}`}
+          target="_blank"
+          rel="noreferrer noopener"
+          className={handles.containerLink}
+        >
+          <img
+            src={logo}
+            width={width}
+            height={height}
+            alt="Logo de WhatsAp"
+            className={handles.containerIcon}
+          />
+          <p className={handles.containerText}>{text}</p>
+        </a>
+      </div>
+    </>
+  )
+}
+
+WhatsappButton.propTypes = {
+  logo: PropTypes.string.isRequired,
+  text: PropTypes.string,
+  phone: PropTypes.string.isRequired,
+  message: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number,
+}
+
+WhatsappButton.defaultProps = {
+  logo: '/arquivos/WhatsappBtn.svg',
+  text: '',
+  phone: '3004507575',
+  message:
+    'Estás comunicándote con VTEX University, por favor ingresa tu duda.',
+  width: 32.05,
+  height: 31.85,
+}
+
+WhatsappButton.schema = {
+  title: 'Botón de WhatsApp',
+  type: 'object',
+  properties: {
+    logo: {
+      title: 'Logo de WhatsApp que se relacione con la marca',
+      type: 'string',
+      widget: {
+        'ui:widget': 'image-uploader',
+      },
+    },
+    text: {
+      title: 'Texto junto al icono de whatsapp',
+      description: 'Agrega el texto que va junto al icono de whatsapp',
+      type: 'string',
+      widget: {
+        'ui:widget': 'textarea',
+      },
+    },
+    phone: {
+      title: 'Teléfono',
+      description: 'Agrega por favor el número de teléfono',
+      type: 'string',
+    },
+    message: {
+      title: 'Mensaje que recibe el cliente',
+      description: 'Agrega por favor el mensaje que se verá para tu cliente',
+      type: 'string',
+      widget: {
+        'ui:widget': 'textarea',
+      },
+    },
+  },
+}
+
+export default WhatsappButton
